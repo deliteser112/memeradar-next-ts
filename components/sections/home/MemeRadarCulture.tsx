4a7a77af88d8cb8e979d671e60f77dc752ab8067
deliteser112 <deliteser112@gmail.com>
2024-04-24 13:24:57 +0200

    fix: culture text size

--- components/sections/home/MemeRadarCulture.tsx
+++ components/sections/home/MemeRadarCulture.tsx
@@ -6,7 +6,7 @@ const MemeRadarCulture: React.FC = () => {
       <img src="/images/black-bar.png" alt="bg pattern" className="w-full min-h-[140px]" />
       <div className="relative max-w-[1280px] mx-auto text-center pt-32">
         <img src="/images/memecoin-symbol.png" alt="meme coin symbol" className="absolute mx-auto top-[-160px] left-[50%] transform -translate-x-1/2" />
-        <p className="text-center text-[50px] text-bold text-white text-[30px] lg:text-[45px]">
+        <p className="text-center text-bold text-white text-[32px] lg:text-[45px]">
           Cultural Movements Decentralized
         </p>
         <p className="text-center text-[18px] lg:text-[22px] text-white my-4">
